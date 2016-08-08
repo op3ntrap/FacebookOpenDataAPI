c4c9fdb2d1c4da2d7629502e045cd4ba50813f49
Risan Raja <op3ntrap@gmail.com>
2016-08-09 00:52:55 +0530

    Update fb_data_extracter.py
    updated threading

--- fb_data_extracter.py
+++ fb_data_extracter.py
@@ -1,79 +1,174 @@
 from pymongo import MongoClient
 import requests
 import time
+from threading import Thread
+import threading
+import Queue
+from thread import start_new_thread
 
-#database initialization
-access_token = "EAACNrjHuFp0BALzPRibHAQANNMt0pTZAkZAWtmZAgBUFBtIvx2RzYUwXZCtbdp6c0z9nNGMAZCBAp302ilvRU3ybtkMsAVwDeGmQDiTOZAJbJggbWo46A96tpoAPeBfUDZA2nlGAc88qsAthGCPSOaNJVWJNkYZAocMZD"
-
-
-def paginator(next_url):
-	#print "executed"
-	data = []
-	while(True):
-		try:
-			dump = requests.get(next_url)
-			feed = dump.json()
-			data = feed['data'] + data
-		except KeyError:
-			break
-	return data
+# database initialization
+access_token = "EAACEdEose0cBANbYxC3ZBMGzq2SxZC0XJpGrk0BBloJHoiqus4C2EbCJvhYYa87ZCQ4gw6UYCcFwAsP8V01ZAJThoe6xLVLEYKUwBxznZA91Gjcor0c4Pt13pVzfz0LjRZAydwouE1bkBmfZCWjFbS5fhp8emt4s3iq1Gt1ZBCZAfTUZC3gjjJYBG6"
 
 
-#feed datarequest
-def feed_request(pageid,until,since):
-	#print "executed"
-	try:
-		data_request = requests.get(
-			"https://graph.facebook.com/v2.7/" + str(pageid) + "/feed?limit=100&since=" + str(since) + "&until=" + str(until) + "&access_token=" + access_token)
-		data = data_request.json()
-		print data
-	except KeyError:
-		return None
-	print "here2"
-	final_data = paginator (data['paging']['next'])
-	final_data = final_data + data['data']
-	return final_data
+def paginator(next_url,queb):
+ 
+    data = []
+    while (True):
+    	#print "executed"
+        try:
+            dump = requests.get (next_url)
+            feed = dump.json ()
+            if (len(feed['data']) == 0):
+                break
 
+            
+            queb.put (feed['data'])
+            #print data
+        except KeyError:
+            break
+ 
+
+
+# feed datarequest
+def feed_request(pageid, until, since):
+    #print "executed"
+    try:
+        data_request = requests.get (
+            "https://graph.facebook.com/v2.7/" + str (pageid) + "/feed?limit=100&since=" + str (
+                since) + "&until=" + str (until) + "&access_token=" + access_token)
+        data = data_request.json ()
+
+        queb.put (data['data'])
+        #print data
+    except KeyError:
+        return None
+    #print "here2"
+    paginator (data['paging']['next'])
+  
+    return final_data
+
+
+def feed_requestv1(pageid, until, since, data_queue):
+    #print "executeda"
+    try:
+        data_request = requests.get (
+            "https://graph.facebook.com/v2.7/" + str (pageid) + "/feed?limit=100&since=" + str (
+                since) + "&until=" + str(until) + "&access_token=" + access_token)
+        data = data_request.json ()
+        if (len(data['data']) == 0):
+        	return
+        data_queue.put (data['data'])
+
+    	#print data
+    except KeyError:
+        return ["N/A"]
+    # #print "here2"
+    try:
+        paginator (data['paging']['next'],data_queue)        
+    except KeyError:
+        return
+    return
+
+
+def get_page_feed(pageid):
+    data_stream = Queue.Queue ()
+    current_time = int (time.time ())
+    c = 7776000
+    dstream = [Thread (target=feed_requestv1, args=[pageid, x, x - c, data_stream]) for x in
+               range (current_time, 1388534400, -7776000)]
+
+    for worker in dstream:
+        worker.start ()
+
+    start_new_thread(drain_timer,(data_stream,pageid,))
+
+    for worker in dstream:
+        worker.join ()
+
+    page_feed = []
+    while not data_stream.empty ():
+        page_feed.append (data_stream.get ())
+    data_stream.join ()
+    return page_feed
+
+def drainer(queb,pageid):
+	strain = []
+	feed_db = MongoClient ().db.page_feed
+	#print "flushing queue"
+
+	while not queb.empty():
+		strain.append(queb.get())
+		#print strain
+		entry = {"pageid": pageid,"feed": strain}        
+		feed_db.insert_one (entry)
+		strain  = []
+
+
+	#entry = { "pageid": pageid,"feed":strain}
+    #eed_db.insert_one(entry)       
+            
+        					
+    
+
+
+def drain_timer(queb,pageid):
+	while (threading.enumerate() > 1):
+		drainer(queb,pageid)
+		time.sleep(3)
+
+		
 
 
 
 
 def get_page_daily_feed(pageid):
-	page_feed = []
-	current_time = time.time()
-	time_constant = 259200
-	current_time = int(time.time())
-	while (current_time>0):
-		try:
-			print "here"
-			initial_data = feed_request(pageid,current_time,(current_time -time_constant))
-			current_time = current_time - time_constant
-			page_feed = page_feed + initial_data
-		except KeyError:
-			break
-	return page_feed
+    page_feed = []
+    current_time = time.time ()
+    time_constant = 86400
+    current_time = int (time.time ())
+    while (current_time > 0):
+        try:
+            #print "here"
+            initial_data = feed_request (pageid, current_time, (current_time - time_constant))
+            current_time = current_time - time_constant
+            page_feed = page_feed + initial_data
+        except KeyError:
+            break
+    return page_feed
 
 
 def run():
-	db = MongoClient().db.fb_locations
-	feed_db = MongoClient().db.page_feed
-	for record in db.find():
-		pageid = record['page_node']
-		page_feed = get_page_daily_feed(pageid)
-		#print page_feed
-		entry = {
-			"pageid" : pageid,
-			"feed" : page_feed
-		}
-		#print "success"
-		feed_db.insert_one(entry)
-		break
-
-run()
+    db = MongoClient ().db.fb_locations
+    feed_db = MongoClient ().db.page_feed
+    for record in db.find ():
+        pageid = record['page_node']
+        page_feed = get_page_daily_feed (pageid)
+        # #print page_feed
+        entry = {
+            "pageid": pageid,
+            "feed": page_feed
+        }
+        # #print "success"
+        feed_db.insert_one (entry)
+        break
 
 
+def run1(pageid):
+    db = MongoClient ().db.fb_locations
+    feed_db = MongoClient ().db.page_feed
+    for record in db.find ():
+        #pageid = record['page_node']
+        page_feed = get_page_feed (pageid)
+        # #print page_feed
+        #entry = {
+           #E "pageid": pageid,
+           # "feed": page_feed
+        #}#
+        # #print "success"
+        #feed_db.insert_one (entry)
+        break
 
-
-
-
-
+for a in MongoClient().db.fb_locations.find():
+	thread.start_new_thread(run1,(a,))
+	time.sleep(900)
+	
