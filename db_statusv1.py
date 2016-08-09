174ea64a285ea2ab82a2be7fb9cd0a992b8d76fc
op3ntrap <op3ntrap@gmail.com>
2016-08-09 18:07:44 +0530

    added ;

--- db_statusv1.py
+++ db_statusv1.py
@@ -1,16 +1,16 @@
 from pymongo import MongoClient
 
-db = MongoClient().db.user_media	
-crsr = db.find()
+db = MongoClient ().db.user_media
+crsr = db.find ()
 
-count = 0 
+count = 0
 c = 0
 
 for a in crsr:
-	try:
-		c+=1
-		count+=len(a['user_media'])
-	except KeyError:
-		continue
-print "doc",c
+    try:
+        c += 1
+        count += len (a['user_media'])
+    except KeyError:
+        continue
+print "doc", c
 print "er", count
--- user_media_extracter.py
+++ user_media_extracter.py
@@ -10,57 +10,55 @@ user_id = []
 
 
 def get_user_recent_media(id):
-    
-    #dump = requests.get(
+    # dump = requests.get(
     #   "https://api.instagram.com/v1/user/media/recent" + id + "?access_token=3407738697.e029fea.8659525c7ca248f3b3fd6eb1130771e1")
     images_list = []
 
     i = 2
     try:
         try:
-            images = requests.get(
-                "https://api.instagram.com/v1/users/"+id+"/media/recent?access_token=3407738697.e029fea.8659525c7ca248f3b3fd6eb1130771e1")
+            images = requests.get (
+                "https://api.instagram.com/v1/users/" + id + "/media/recent?access_token=3407738697.e029fea.8659525c7ca248f3b3fd6eb1130771e1")
             ##print  "https://api.instagram.com/v1/users/"+id+"/media/recent?access_token=3407738697.e029fea.8659525c7ca248f3b3fd6eb1130771e1"
-            url =  "https://api.instagram.com/v1/users/"+id+"/media/recent?access_token=3407738697.e029fea.8659525c7ca248f3b3fd6eb1130771e1"
+            url = "https://api.instagram.com/v1/users/" + id + "/media/recent?access_token=3407738697.e029fea.8659525c7ca248f3b3fd6eb1130771e1"
 
-            data = images.json()
+            data = images.json ()
         except JSONDecodeError:
-            #print "json error"
+            # print "json error"
             return images_list
         for values in data['data']:
-            images_list.append(values)
+            images_list.append (values)
     except KeyError:
-        #print "empty data"
+        # print "empty data"
         return images_list
     #############################################################
     while (i < 5):
-    	try:
-    		max_id = data['pagination']['next_max_id']
-    	except KeyError as e :
-    		print "max_id"
-    		break
-
         try:
-            images2 = requests.get(url+ "&max_id=" +max_id)
-            #print url+ "&max_id=" + data['pagination']['next_max_id']
-            ##print images2.content
-            #print "paginated 1 "
-
-            time.sleep(7)
-            #print "sleeping"
-
-            data = images2.json()
-        # #print data['pagination']['next_url']
-        except KeyError:
-            #print "pagination empty data returned"
+            max_id = data['pagination']['next_max_id']
+        except KeyError as e:
+            print "max_id"
             break
 
-       
+        try:
+            images2 = requests.get (url + "&max_id=" + max_id)
+            # print url+ "&max_id=" + data['pagination']['next_max_id']
+            ##print images2.content
+            # print "paginated 1 "
+
+            time.sleep (7)
+            # print "sleeping"
+
+            data = images2.json ()
+        # #print data['pagination']['next_url']
+        except KeyError:
+            # print "pagination empty data returned"
+            break
+
         for values in data['data']:
-            images_list.append(values)
-        # #print len(images_list)
+            images_list.append (values)
+            # #print len(images_list)
     ##############################################################
-    #print "finished with ", len(images_list)
+    # print "finished with ", len(images_list)
     return images_list
 
 
@@ -73,39 +71,38 @@ def user_existence(id):
 
 
 def get_location_buzz(location_id):
-    location_buzz = ibuzz.find_one({'id': location_id})
+    location_buzz = ibuzz.find_one ({'id': location_id})
     return location_buzz['buzz']
 
 
 ##executing script
-client = MongoClient()
+client = MongoClient ()
 db = client.db
 ibuzz = db['instagram_buzz']
 
 locationids = []
 
-for value in ibuzz.find():
-    locationids.append(value['id'])
+for value in ibuzz.find ():
+    locationids.append (value['id'])
 
 user_media_db = db['user_media']
-for users in user_media_db.find():
-	user_id.append(users['id'])
+for users in user_media_db.find ():
+    user_id.append (users['id'])
 
 for location in locationids:
-    media_in_location = get_location_buzz(location)
+    media_in_location = get_location_buzz (location)
     for media in media_in_location:
-    	usr = media['user']['id']
-    	if (usr in user_id):
-    		print "continuing"
-    		continue
-        
-        
-        user_media = get_user_recent_media(media['user']['id'])
-   
+        usr = media['user']['id']
+        if (usr in user_id):
+            print "continuing"
+            continue
+
+        user_media = get_user_recent_media (media['user']['id'])
+
         entry = {
             "id": media['user']['id'],
             "user_media": user_media
         }
 
-        user_media_db.insert_one(entry)
-        user_id.append(id)
+        user_media_db.insert_one (entry)
+        user_id.append (id)
