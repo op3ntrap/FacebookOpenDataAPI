61fd0e023787142c20cd801c5f54b5ad021ab0a1
Risan Raja <op3ntrap@gmail.com>
2016-08-09 07:21:01 +0530

    Update dbstatus_fb.py

--- dbstatus_fb.py
+++ dbstatus_fb.py
@@ -2,5 +2,8 @@ from pymongo import MongoClient
 db= MongoClient().db.page_feed
 count = 0
 for a in db.find():
-	count+=len(a['feed'][0])
+	try:
+		count+=len(a['feed'][0])
+	except IndexError:
+		print a['pageid']
 print count
