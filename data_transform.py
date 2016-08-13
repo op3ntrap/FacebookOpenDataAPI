f5e5e8a15d0684782052679a781097309c2e18ad
op3ntrap <op3ntrap@gmail.com>
2016-08-13 18:06:37 +0530

    added ;

---
+++ data_transform.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient as mg
+client = mg()
+
+
+def dispose(array_r,page_id):
+    new_db = client.fb_feed[page_id]
+    if len (array_r) > 2:
+        for a in array_r:
+            new_db.insert_one (a)
+    if len (array_r) == 1:
+        for feed in array_r[0]:
+            new_db.insert_one(feed)
+
+
+
+for a in client.db:
+    dispose (a['feed'], a['pageid'])
